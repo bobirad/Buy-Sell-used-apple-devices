791fc607bf3b48e0cce15b07ae849beea6f07108
Bogdan Radenkov <104485957+bobirad@users.noreply.github.com>
2022-12-07 12:06:17 +0200

    loginview added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material-model';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [CommonModule, MaterialModule]
 })
 export class LoginComponent {
 
