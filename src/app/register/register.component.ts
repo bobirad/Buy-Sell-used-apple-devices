73e49b72540085c97b4406cf876b281bb6eab252
Bogdan Radenkov <104485957+bobirad@users.noreply.github.com>
2022-12-07 12:36:15 +0200

    registerview-added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material-model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  standalone: true,
+  imports: [CommonModule, MaterialModule]
+
 })
 export class RegisterComponent {
 
