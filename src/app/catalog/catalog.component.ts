e100f59df2723747b81e1782a4d19e6d554a2042
Bogdan Radenkov <104485957+bobirad@users.noreply.github.com>
2022-12-14 00:15:32 +0200

    updates

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../interfaces/item';
 import { DataService } from '../shared/data.service';
 
 @Component({
@@ -6,6 +7,37 @@ import { DataService } from '../shared/data.service';
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  itemsList: Item[] = [];
+  itemObj: Item = {
+    device: '',
+    model: '',
+    year: NaN,
+    imageUrl: '',
+    price: NaN,
+    description: '',
+  };
+  device: string = '';
+  model: string = '';
+  year: number = NaN;
+  imageUrl: string = '';
+  price: number = NaN;
+  description: string = '';
+  constructor(private db: DataService) {}
+
+  errorFetcingData = false;
+
+
+  ngOnInit(): void {
+    this.db.GetAllListings().subscribe({
+      next: (value: Item[]) => {
+        this.itemsList = value;
+      },
+      error: (err: any) => {
+        this.errorFetcingData = true;
+        console.error(err);
+      }
+    });
+  }
 
 }
