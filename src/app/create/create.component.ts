10e1603812732c8707845f6c87ce6d4ebae7c7a4
Bogdan Radenkov <104485957+bobirad@users.noreply.github.com>
2022-12-20 22:31:56 +0200

    validation updates

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -4,16 +4,16 @@ import { Router } from '@angular/router';
 import { Item } from '../interfaces/item';
 import { getAuth } from "firebase/auth";
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
 })
-export class CreateComponent implements OnInit{
+export class CreateComponent implements OnInit {
+
   auth = getAuth();
-  createForm!:FormGroup;
-  
   itemObj: Item = {
     id: '',
     device: '',
@@ -24,47 +24,97 @@ export class CreateComponent implements OnInit{
     description: '',
     owner: ''
   };
-  id: string = '';
-  device: string ='';
-  model: string = '';
-  year: number = NaN;
-  imageUrl: string = '';
-  price: number = NaN;
-  description: string = '';
-  owner: any = '';
+  createForm!: FormGroup;
+  device: any;
+  model: any;
+  year: any;
+  imageUrl: any;
+  price: any;
+  description: any;
+
 
   constructor(
     public dataService: DataService,
-    public router: Router
-  ){ }
-
-  ngOnInit() {
-    this.dataService.getAllListings();
+    private router: Router
+  ) { }
+  ngOnInit(): void {
     this.createForm = new FormGroup({
       device: new FormControl('', [Validators.required, Validators.minLength(3)]),
       model: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      year: new FormControl('', [Validators.required]),
-      imageUrl: new FormControl('', [Validators.required]),
-      price: new FormControl('', [Validators.required]),
-      description: new FormControl('', [Validators.required, Validators.minLength(3)])
+      year: new FormControl('', Validators.required),
+      imageUrl: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      price: new FormControl('', Validators.required),
+      description: new FormControl('', [Validators.required, Validators.minLength(10)]),
     })
   }
-
-  addListing(){
-    if(this.createForm.invalid){ return; }
-    let creator = this.auth.currentUser;
-    const creatorEmail = creator?.email;
-    this.itemObj.id = '';
-    this.itemObj.device = this.device;
-    this.itemObj.model = this.model;
-    this.itemObj.year = this.year;
-    this.itemObj.imageUrl = this.imageUrl;
-    this.itemObj.price = this.price;
-    this.itemObj.description = this.description;
+  addListing() {
+    this.itemObj = this.createForm.value;
+    const creatorEmail = this.auth.currentUser?.email;
     this.itemObj.owner = creatorEmail!.toString();
-    this.dataService.createListing(this.itemObj);
-    this.router.navigate(['catalog']);
+    this.itemObj.id = '';
     
-    
+    if (this.createForm.invalid) { return; }
+    else {
+      this.dataService.createListing(this.itemObj);
+      this.router.navigate(['catalog']);
+    }
+
   }
+
+
+
+
+
+  /*
+    auth = getAuth();
+    createForm!: FormGroup;
+    itemObj: any;
+  
+    id!: string;
+    device!: string;
+    model!: string;
+    year!: number;
+    imageUrl!: string;
+    price!: number;
+    description!: string;
+    owner!: any;
+  
+    constructor(
+      public dataService: DataService,
+      public router: Router
+    ){ }
+  
+    ngOnInit() {
+      this.createForm = new FormGroup({
+        device: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        model: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        year: new FormControl('', [Validators.required]),
+        imageUrl: new FormControl('', [Validators.required]),
+        price: new FormControl('', [Validators.required]),
+        description: new FormControl('', [Validators.required, Validators.minLength(3)])
+      })
+    }
+  
+    addListing(){
+      alert('submited');
+      const {device, model, year, imageUrl, price, description} = this.createForm.value;
+  
+      if(this.createForm.invalid){ return; }
+      const creatorEmail = this.auth.currentUser?.email;
+      this.itemObj.id = '';
+      this.itemObj.device = device;
+      this.itemObj.model = model;
+      this.itemObj.year = year;
+      this.itemObj.imageUrl = imageUrl;
+      this.itemObj.price = price;
+      this.itemObj.description = description;
+      this.itemObj.owner = creatorEmail!.toString();
+      this.dataService.createListing(this.itemObj);
+      this.router.navigate(['catalog']);
+      
+      
+    }
+  
+  */
+
 }
